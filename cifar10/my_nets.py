8dd3291eea37ba167367f8b4d07c93b68afc80dd
Jean-Marc Gervais <jm.gervais@free.fr>
2020-07-09 16:46:00 +0200

    Minor modif.

--- cifar10/my_nets.py
+++ cifar10/my_nets.py
@@ -1,13 +1,9 @@
 import numpy as np
 
 import torch
-#import torchvision
-#import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-#from torchvision.models import *
-#from collections import OrderedDict
 
 # ===== Ex. from tutorial  =====
 # https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
